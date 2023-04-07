a6ac6cbc97f55f5fc839c8255c7f15c5b86435a8
seungyeol lee <misracis@gmail.com>
2023-04-07 23:51:15 +0900

    add pagination of pageable in grade service

--- src/main/java/com/example/ecommerce/global/member/service/GradeService.java
+++ src/main/java/com/example/ecommerce/global/member/service/GradeService.java
@@ -1,10 +1,15 @@
 package com.example.ecommerce.global.member.service;
 
-import com.example.ecommerce.global.member.model.*;
+import com.example.ecommerce.global.member.model.Customer;
+import com.example.ecommerce.global.member.model.MemberGrade;
+import com.example.ecommerce.global.member.model.MemberRole;
+import com.example.ecommerce.global.member.model.Seller;
 import com.example.ecommerce.global.member.model.dto.GradeDto;
 import com.example.ecommerce.global.member.repository.CustomerRepository;
 import com.example.ecommerce.global.member.repository.SellerRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -21,9 +26,11 @@ public class GradeService {
     //MemberGrade별 회원 조회
     public List<GradeDto.ResponseDto> getGradeList(MemberGrade memberGrade, MemberRole memberRole){
         List<GradeDto.ResponseDto> responseList = new ArrayList<>();
+        int page = 1;
 
         if(memberRole == MemberRole.CUSTOMER){
-            List<Customer> CustomerList = new ArrayList<>(customerRepository.findByMemberGrade(memberGrade));
+            PageRequest pageRequest = PageRequest.of(page - 1, 10, Sort.by(Sort.Direction.DESC, "purchaseAmount"));
+            List<Customer> CustomerList = new ArrayList<>(customerRepository.findByMemberGrade(memberGrade, pageRequest));
             for (Customer customer : CustomerList) {
                 responseList.add(new GradeDto.ResponseDto(customer));
             }
